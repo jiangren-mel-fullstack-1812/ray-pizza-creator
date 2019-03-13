7d9ca1a04b974488b63e57fb67704a059366862a
yuetongfeng <ryfprivate@outlook.com>
2019-03-13 20:41:34 +1100

    Added orderItem model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './product.model';
 export * from './order.model';
+export * from './order-item.model';
---
+++ src/models/order-item.model.ts
@@ -0,0 +1,19 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model()
+export class OrderItem extends Model {
+  @property({
+    type: 'string',
+  })
+  productId?: string;
+
+  @property({
+    type: 'number',
+  })
+  quantity?: number;
+
+
+  constructor(data?: Partial<OrderItem>) {
+    super(data);
+  }
+}
